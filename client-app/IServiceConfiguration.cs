644d2e4d2307f2e7f9a44dd3f30ef64db6fda137
Kohai <HighTechKohai@gmail.com>
2024-07-21 16:48:39 -0400

    No longer include unnecessary header in new files

--- client-app/IServiceConfiguration.cs
+++ client-app/IServiceConfiguration.cs
@@ -1,8 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,29 +7,8 @@ using Microsoft.Extensions.Configuration;
 
 namespace client_app
 {
-    public interface IServiceConfiguration
+    public interface IService<TConfig> where TConfig : new()
     {
-
-
-        public static T LoadConfiguration<T>(string sectionName) where T : new()
-        {
-            var configInstance = new T();
-
-            // Create switch mappings for environment variables
-            var switchMappings = ConfigurationUtils.CreateSwitchMappings<T>(sectionName);
-
-            // Build section-specific configuration
-            var sectionConfiguration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .AddEnvironmentVariables(prefix: $"{sectionName}__")
-                .AddCommandLine(Environment.GetCommandLineArgs(), switchMappings)
-                .Build();
-
-            // Bind the combined configuration to the instance
-            sectionConfiguration.GetSection(sectionName).Bind(configInstance);
-
-            return configInstance;
-
-        }
+        TConfig ServiceConfiguration { get; }
     }
+}
